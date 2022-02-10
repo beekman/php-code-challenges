47096836c4fcb799bf72e0128f221a67dfc0fbb4
Ben Beekman <ben@riotnewmedia.com>
2022-02-09 19:56:15 -0800

    solution for rotleft

---
+++ rotleft.php
@@ -0,0 +1,64 @@
+<?php
+
+
+/*
+A left rotation operation on an array shifts each of the array's elements  unit to the left. For example, if  left rotations are performed on array , then the array would become . Note that the lowest index item moves to the highest index in a rotation. This is called a circular array.
+
+Given an array  of  integers and a number, , perform  left rotations on the array. Return the updated array to be printed as a single line of space-separated integers.
+
+Function Description
+
+Complete the function rotLeft in the editor below.
+
+rotLeft has the following parameter(s):
+
+int a[n]: the array to rotate
+int d: the number of rotations
+Returns
+
+int a'[n]: the rotated array
+Input Format
+
+The first line contains two space-separated integers  and , the size of  and the number of left rotations.
+The second line contains  space-separated integers, each an .
+
+Constraints
+
+Sample Input
+
+5 4
+1 2 3 4 5
+Sample Output
+
+5 1 2 3 4
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+function rotLeft($a, $d) {
+	$arr = array_slice($a, $d);
+	$arr = array_merge($arr, array_slice($a, 0, $d));
+	return $arr;
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$first_multiple_input = explode(' ', rtrim(fgets(STDIN)));
+
+$n = intval($first_multiple_input[0]);
+
+$d = intval($first_multiple_input[1]);
+
+$a_temp = rtrim(fgets(STDIN));
+
+$a = array_map('intval', preg_split('/ /', $a_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = rotLeft($a, $d);
+
+fwrite($fptr, implode(" ", $result) . "\n");
+
+fclose($fptr);
